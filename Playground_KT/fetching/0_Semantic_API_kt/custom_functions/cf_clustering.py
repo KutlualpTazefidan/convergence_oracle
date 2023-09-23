311d502860b825f7459630085ec3f99918f409c9
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-23 18:32:04 +0200

    Make plots

--- Playground_KT/fetching/0_Semantic_API_kt/custom_functions/cf_clustering.py
+++ Playground_KT/fetching/0_Semantic_API_kt/custom_functions/cf_clustering.py
@@ -1,3 +1,25 @@
+import pandas as pd
+import ast
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from sentence_transformers import SentenceTransformer
+from bertopic import BERTopic
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import time
+from umap import UMAP
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from scipy.spatial.distance import euclidean
+from bertopic.representation import KeyBERTInspired
+import plotly.graph_objs as go
+# This package allows me to save the plots into html file
+import plotly.offline as pyo
+from plotly.express.colors import sample_colorscale
+
+
 # Function to load and clean the dataset
 def load_clean_dataset(path,years=None):
     # Load the dataset from the specified path
@@ -20,7 +42,7 @@ def load_clean_dataset(path,years=None):
 
     def clean_text(row):
         # Combine 'title' and 'abstract' into a single column
-        combined_text = row['title'] + '. ' + row['abstract']
+        combined_text = row['title'] + ' ' + row['abstract']
 
         # Check if 'fieldsOfStudy' exists and is not empty
         if row['fieldsOfStudy']:
@@ -29,8 +51,8 @@ def load_clean_dataset(path,years=None):
             # Remove single quotes from each field of study
             cleaned_fields = [field.strip(" '") for field in fields_list]
             # Join the cleaned fields with commas and spaces
-            field_of_study_str = ', '.join(cleaned_fields)
-            combined_text += '. ' + field_of_study_str
+            field_of_study_str = ' '.join(cleaned_fields)
+            combined_text += ' ' + field_of_study_str
 
         # Tokenize and remove stopwords
         cleaned_tokens = [word for word in combined_text.split() if word.lower() not in ENGLISH_STOP_WORDS]
@@ -44,12 +66,12 @@ def load_clean_dataset(path,years=None):
     df_c = df_c.sample(frac=1).reset_index(drop=True)
     
     print("Length of the dataset: ", len(df_c))
-    print("Null in the dataset",df.isnull().sum())
+    # print("Null in the dataset",df.isnull().sum())
     # Return the cleaned DataFrame
     return df_c
 
 # Function to cluster data using BERTopic
-def cluster_data(df,min_topic_size=50,fine_tune_label=False,get_topics=False,get_proba=False):  
+def cluster_data(df,min_topic_size=50,fine_tune_label=False,get_topics=False,get_proba=False,ngram=2):  
     # Extract documents from the 'title_abstract_studyfield' column
     docs = df['title_abstract_studyfield'].tolist()
     
@@ -59,9 +81,9 @@ def cluster_data(df,min_topic_size=50,fine_tune_label=False,get_topics=False,get
     # Create and fit a BERTopic model
     if fine_tune_label:
         representation_model = KeyBERTInspired()
-        model = BERTopic(language="english", n_gram_range=(1, 2), min_topic_size=min_topic_size,representation_model=representation_model)
+        model = BERTopic(language="english", n_gram_range=(1, ngram), min_topic_size=min_topic_size,representation_model=representation_model)
     else:
-        model = BERTopic(language="english", n_gram_range=(1, 2), min_topic_size=min_topic_size)
+        model = BERTopic(language="english", n_gram_range=(1, ngram), min_topic_size=min_topic_size)
     
     topics, proba = model.fit_transform(docs)
     
@@ -117,7 +139,7 @@ def add_topic_labels(df,topics,proba,model):
     display(df.head())
     return df
 
-def visualize_intertopic_distances_3d(df,topics,model,umap_embeddings):
+def visualize_intertopic_distances_3d(filename_prefix,df,topics,model,umap_embeddings):
     
     # Step 1: Reduce Embeddings to 3D using UMAP
     def reduce_to_3d(embeddings):
@@ -161,36 +183,94 @@ def visualize_intertopic_distances_3d(df,topics,model,umap_embeddings):
 
         # Now you can calculate topic counts using np.bincount
         topic_counts = np.bincount(topics_shifted)
+                
+        # Calculate the sphere radius based on topic count
+        sphere_radius = np.cbrt(topic_counts)
+        
+        # Calculate the sphere radius based on topic count
+        max_radius = np.max(sphere_radius)
+        min_radius = np.min(sphere_radius)
+        normalized_radius = (sphere_radius - min_radius) / (max_radius - min_radius)
+        
+        rgb_colors = sample_colorscale('Viridis_r', list(normalized_radius))
         
         # Create a scatter plot with points for each topic centroid
         fig = go.Figure()
         for i, centroid in enumerate(centroids):
-            # Calculate the sphere radius based on topic count
-            sphere_radius = np.cbrt(topic_counts[i])
-            
+            # Capitalize the first letter of the topic label
+            capitalized_label = best_labels[i].capitalize()
+            if i == len(normalized_radius)-1: 
+                hideScale = True
+            else:
+                hideScale = False
+                
             # Create a sphere marker for each topic
             fig.add_trace(go.Scatter3d(
                 x=[centroid[0]],
                 y=[centroid[1]],
                 z=[centroid[2]],
                 mode='markers',
-                marker=dict(size=sphere_radius, color='blue', opacity=0.5),
-                text=f'Topic {best_labels[i]}, Count: {topic_counts[i]}'
+                marker=dict(
+                    size=sphere_radius[i]*5, 
+                    color= rgb_colors[i],
+                    colorscale='Viridis_r',
+                    opacity=0.6,
+                    colorbar=dict(
+                        title="Citation Count",
+                        yanchor="top",
+                        y=1.1,
+                        xanchor="left",
+                        x=-0.065,
+                        # ticks="outside",
+                    ),
+                    cmin=min(topic_counts),
+                    cmax=max(topic_counts),
+                    showscale=hideScale
+                ),
+                name=f'{capitalized_label}, Count: {topic_counts[i]}',
+                text=f'Topic {capitalized_label}, Count: {topic_counts[i]}'
             ))
-            
-        # Customize the color scale if needed
-        color_scale = px.colors.sequential.Plasma  # Replace with a desired color scale
-        color_variable = topic_counts  # Replace 'petal_length' with the variable you want to use
-        fig.update_traces(marker=dict(colorbar=dict(title='Color Bar Title', tickvals=[min(color_variable), max(color_variable)], ticktext=['Min', 'Max']), colorscale=color_scale))
-
-        # Customize the layout if needed
-        fig.update_layout(scene=dict(aspectmode='cube'))
+        # fig.update_layout(legend_orientation="h")
         # Customize the layout and axis labels
-        fig.update_layout(scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Z'))
+        fig.update_layout(
+            scene=dict(
+                aspectmode='cube',
+                xaxis_title='', 
+                yaxis_title='', 
+                zaxis_title='',
+                xaxis=dict(
+                    showticklabels=False,  # Disable tick labels on the x-axis
+                ),
+                yaxis=dict(
+                    showticklabels=False,  # Disable tick labels on the y-axis
+                ),
+                zaxis=dict(
+                    showticklabels=False,  # Disable tick labels on the z-axis
+                ),
+                # camera=dict(
+                #     eye=dict(x=0.1, y=0, z=1.5)  # Set the initial camera position
+                # )
+                # dragmode='orbit',  # Limit interaction to orbit mode
+            ),
+            coloraxis=dict(
+                colorbar=dict(
+                    title="Citation Count",
+                    showticklabels=False,  # Hide colorbar ticks
+
+                )
+            ),
+            title='Distance for Topic Clusters'  # Add your desired title here
+        )
         
         # Show the 3D scatter plot
+        pyo.plot(fig,filename="./plots/3d_distance_map_for_"+str(filename_prefix)+"_"+str(len(topics))+"_topics.html",auto_open=False)
         fig.show()
         
+    # # Customize the color scale if needed
+    # color_scale = px.colors.sequential.Plasma  # Replace with a desired color scale
+    # color_variable = topic_counts  # Replace 'petal_length' with the variable you want to use
+    # fig.update_traces(marker=dict(colorbar=dict(title='Color Bar Title', tickvals=[min(color_variable), max(color_variable)], ticktext=['Min', 'Max']), colorscale=color_scale))
+
     # Assuming you have your embeddings, topics, and model
     print("Step 1/3: Reducing the dimensions of the embeddings ...")
     umap_embeddings = reduce_to_3d(umap_embeddings)  # Step 1
